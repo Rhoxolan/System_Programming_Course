f70736d59ae5ac6637156c07748d4d91e597e75d
Rhoxolan <pba17mail@gmail.com>
2022-08-30 13:13:04 +0300

    Add PW

---
+++ 2022.08.26_PW/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _2022._08._26_PW
+{
+    internal class Program
+    {
+        Mutex mutexObj = new();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        //Ты тут. Просмотреть статью на метаните и написать первое задание.
+    }
+}
