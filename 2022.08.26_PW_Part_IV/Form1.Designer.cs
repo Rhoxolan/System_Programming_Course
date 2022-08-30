f7a816df2232d0eed11c513b1238d9182fa604eb
Rhoxolan <pba17mail@gmail.com>
2022-08-30 23:22:24 +0300

    Add to PW

--- 2022.08.26_PW_Part_IV/Form1.Designer.cs
+++ 2022.08.26_PW_Part_IV/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -40,14 +44,56 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Задание 6:";
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(93, 169);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(111, 56);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(93, 169);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Задание 7:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +102,9 @@
         #endregion
 
         private Label label1;
+        private ListBox listBox1;
+        private Button button1;
+        private ListBox listBox2;
+        private Label label2;
     }
 }
--- 2022.08.26_PW_Part_IV/Form1.cs
+++ 2022.08.26_PW_Part_IV/Form1.cs
@@ -1,10 +1,73 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+
 namespace _2022._08._26_PW_Part_IV
 {
     public partial class Form1 : Form
     {
+        Mutex mutexObj;
+        BindingList<int> list1;
+        BindingList<int> list2;
+
+        //Напоминание: попробовать сделать задание 7 через Семафор
+
         public Form1()
         {
             InitializeComponent();
+            mutexObj = new();
+            list1 = new();
+            list2 = new();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.DataSource = list1;
+            listBox2.DataSource = list2;
+        }
+
+        private void Print20Symbols()
+        {
+            mutexObj.WaitOne();
+            try
+            {
+                for (int i = 0; i < 21; i++)
+                {
+                    list1.Add(i);
+                    Thread.Sleep(10);
+                }
+            }
+            finally
+            {
+                mutexObj.ReleaseMutex();
+            }
+        }
+
+        private void Print10Symbols()
+        {
+            mutexObj.WaitOne();
+            try
+            {
+                for (int i = 11 - 1; i >= 0; i--)
+                {
+                    list2.Add(i);
+                    Thread.Sleep(10);
+                }
+            }
+            finally
+            {
+                mutexObj.ReleaseMutex();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list1.Clear();
+            list2.Clear();
+            Thread thread1 = new(Print20Symbols);
+            Thread thread2 = new(Print10Symbols);
+            thread1.Start();
+            Thread.Sleep(1);
+            thread2.Start();
         }
     }
 }
