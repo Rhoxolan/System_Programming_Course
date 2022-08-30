1c6f44c24df06b3d738d754a753fd9cfb11b5dc6
Rhoxolan <pba17mail@gmail.com>
2022-08-30 21:31:41 +0300

    Add to PW

---
+++ 2022.08.26_PW_Part_III/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _2022._08._26_PW_Part_III
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Thread> threads = new();
+            for (int i = 0; i < 10; i++)
+            {
+                threads.Add(new(ShowRandomNumbers) { Name = $"Lists thread {i}" });
+            }
+            Semaphore semaphore = new(3, 3);
+            foreach (Thread thread in threads)
+            {
+                thread.Start(semaphore);
+            }
+        }
+
+        static void ShowRandomNumbers(object? obj)
+        {
+            Semaphore? semaphore = obj as Semaphore;
+            semaphore?.WaitOne();
+            try
+            {
+                Random random = new();
+                int[] nums = new int[3] { random.Next(0, 101), random.Next(0, 101), random.Next(0, 101) };
+                Console.WriteLine($"Поток {Environment.CurrentManagedThreadId} выводит числа {nums[0]}, {nums[1]} и {nums[2]}");
+                Thread.Sleep(500);
+            }
+            finally
+            {
+                semaphore?.Release();
+            }
+            
+        }
+    }
+}
