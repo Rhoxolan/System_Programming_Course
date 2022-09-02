1de1e3eae595bd827eb077498756fc29c9077546
Rhoxolan <pba17mail@gmail.com>
2022-09-02 23:26:18 +0300

    Add HW

---
+++ 2022.08.31_HW/Bus.cs
@@ -0,0 +1,30 @@
+﻿namespace _2022._08._31_HW
+{
+    public class Bus
+    {
+        private int capacity;
+
+        public string Name { get; set; }
+
+        public Bus(int capacity, string name)
+        {
+            this.capacity = capacity;
+            Name = name;
+        }
+
+        public Bus(int capacity)
+        {
+            this.capacity = capacity;
+        }
+
+        public void TakePeople(int People)
+        {
+            if(capacity > People)
+            {
+                People = 0;
+                return;
+            }
+            People -= capacity;
+        }
+    }
+}
---
+++ 2022.08.31_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2022._08._31_HW
+{
+    internal class Program
+    {
+        static List<Bus> buses = new()
+        {
+            new(25) {Name = "Маршрутка 230"},
+            new(50) {Name = "Троллейбус 8"},
+            new(55) {Name = "Автобус C"}
+        };
+
+        static void Main()
+        {
+
+        }
+    }
+}
