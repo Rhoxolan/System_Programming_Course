85e61b33bda2677ed1c7e23a67d0acbcd28454d2
Rhoxolan <pba17mail@gmail.com>
2022-09-03 20:35:23 +0300

    Add to PW

--- 2022.08.31_PW/Philosopher.cs
+++ 2022.08.31_PW/Philosopher.cs
@@ -4,14 +4,18 @@
     {
         public string Name { get; set; }
 
-        public void Eating()
+        public void Eating(Fork fork1, Fork fork2)
         {
-            Thread.Sleep(5000);
+            Console.WriteLine($"Философ {Name} кушает");
+            //Thread.Sleep(5000);
+            fork1.IsBusy = false;
+            fork2.IsBusy = false;
         }
 
         public void Thinking()
         {
-            Thread.Sleep(5000);
+            Console.WriteLine($"Философ {Name} думает");
+            //Thread.Sleep(5000);
         }
     }
 }
--- 2022.08.31_PW/Program.cs
+++ 2022.08.31_PW/Program.cs
@@ -1,8 +1,11 @@
-﻿namespace _2022._08._31_PW
+﻿//Решение заключается в ограничении количества человек, которые могут подойти к месту, где лежат вилки. Это решение уходит
+//от максималньного быстродействия, но гарантирует, что все философы рано или поздно смогут поесть.
+
+namespace _2022._08._31_PW
 {
     internal class Program
     {
-        static Semaphore semaphore = new(2,2);
+        static object locker = new();
         static List<Fork> forks = new() { new(), new(), new(), new(), new() };
         static List<Philosopher> philosophers = new()
         {
@@ -16,59 +19,61 @@
         static void Main()
         {
             Task.Run(ExitWait);
-            while (true)
+            List<Task> tasksCycles = new();
+            foreach(Philosopher philosopher in philosophers)
             {
-                List<Task> tasks = new();
-                semaphore.WaitOne();
-                foreach (Philosopher philosopher in philosophers)
-                {
-                    Fork fork1 = null;
-                    Fork fork2 = null;
-                    for (int i = 0; i < forks.Count; i++)
-                    {
-                        if (!forks[i].IsBusy)
-                        {
-                            fork1 = forks[i];
-                            break;
-                        }
-                    }
-                    for (int i = 0; i < forks.Count; i++)
-                    {
-                        if (!forks[i].IsBusy)
-                        {
-                            fork2 = forks[i];
-                            break;
-                        }
-                    }
-                    if (fork1 != null && fork2 != null)
-                    {
-                        tasks.Add(Task.Run(() => Cycle(philosopher, fork1, fork2)));
-                    }
-                }
-                Task.WaitAll(tasks.ToArray());
-                semaphore.Release();
+                tasksCycles.Add(new(()=> Cycle(philosopher)));
             }
+            foreach(Task task in tasksCycles)
+            {
+                task.Start();
+            }
+            Task.WaitAll(tasksCycles.ToArray());
         }
 
-        static void Cycle(Philosopher philosopher, Fork fork1, Fork fork2)
+        static void Cycle(Philosopher philosopher)
         {
-            Console.WriteLine($"Философ {philosopher.Name} думает");
-            philosopher.Thinking();
-            fork1.IsBusy = true;
-            fork2.IsBusy = true;
-            Console.WriteLine($"Философ {philosopher.Name} кушает");
-            philosopher.Eating();
-            fork1.IsBusy = false;
-            fork2.IsBusy = false;
-            Console.WriteLine($"Философ {philosopher.Name} думает");
-            philosopher.Thinking();
+            while (true)
+            {
+                philosopher.Thinking();
+                bool goEat = false;
+                Fork leftFork = null;
+                Fork rightFork = null;
+                lock (locker)  //К выдаче вилок подходит может подойти только 1 человек!
+                {
+                    foreach (Fork fork1 in forks) //Ищем первую вилку
+                    {
+                        if (!fork1.IsBusy) //Если есть свободная вилка
+                        {
+                            fork1.IsBusy = true; //Берём её
+                            foreach (Fork fork2 in forks) //Ищем вторую вилку
+                            {
+                                if (!fork2.IsBusy) //Если есть свободная вилка
+                                {
+                                    fork2.IsBusy = true; //Берём её
+                                    goEat = true; //Идём кушать
+                                    leftFork = fork1;
+                                    rightFork = fork2;
+                                    break;
+                                }
+                            }
+                            fork1.IsBusy = false; //Если не была найдена вторая вилка - ложим вилку и останавливаем цикл
+                            break;
+                        }
+                    }
+                }
+                if (goEat) //А вот кушать за общим столом могут все вместе!
+                {
+                    philosopher.Eating(leftFork, rightFork); //Вилки ложаться обратно внутри класса
+                }
+            }
         }
 
         static void ExitWait()
         {
             while (true)
             {
-                if (Console.ReadKey().Key == ConsoleKey.Q)
+                if (Console.ReadKey().Key == ConsoleKey.End)
                 {
                     Environment.Exit(0);
                 }
