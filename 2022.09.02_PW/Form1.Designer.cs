bde78326af2a39ce5272d43c6a2940dde83e76f4
Rhoxolan <pba17mail@gmail.com>
2022-09-06 22:57:32 +0300

    Add to PW

--- 2022.09.02_PW/Form1.Designer.cs
+++ 2022.09.02_PW/Form1.Designer.cs
@@ -33,6 +33,20 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -75,11 +89,147 @@
             this.textBox3.Size = new System.Drawing.Size(151, 23);
             this.textBox3.TabIndex = 5;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Задание 2:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 187);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(156, 244);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(171, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Задание 2:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(171, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Run";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(174, 113);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(182, 274);
+            this.listBox2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(182, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Количество выведенных чисел:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(174, 408);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(182, 23);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(174, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Мин:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(174, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Макс:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(219, 56);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 16;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(219, 85);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown2.TabIndex = 17;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
@@ -87,6 +237,8 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,5 +250,17 @@
         private Button button1;
         private TextBox textBox2;
         private TextBox textBox3;
+        private Label label2;
+        private Button button2;
+        private ListBox listBox1;
+        private Label label3;
+        private Button button3;
+        private ListBox listBox2;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private Label label6;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
     }
 }
--- 2022.09.02_PW/Form1.cs
+++ 2022.09.02_PW/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace _2022._09._02_PW
 {
     public partial class Form1 : Form
@@ -10,7 +12,52 @@ namespace _2022._09._02_PW
         private void button1_Click(object sender, EventArgs e)
         {
             Task task1 = new(() => { textBox1.Text = DateTime.Now.ToString(); });
-            task1.Start(); //Подумать, может выйдет все это запустить через паралел.
+            Parallel.Invoke(
+                task1.Start,
+                () => Task.Factory.StartNew(() => { textBox2.Text = DateTime.Now.ToString(); }),
+                () => Task.Run(() => { textBox3.Text = DateTime.Now.ToString(); }));
+        }
+
+        private bool IsSimple(int num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            await Task.Run(() => { Parallel.For(1, 1000, (i) => { if (IsSimple(i)) { AddToListBox(listBox1, i); } }); });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            int counter = 0;
+            Task.WaitAll(Task.Run(() => { Parallel.For((int)numericUpDown1.Value, (int)numericUpDown2.Value, (i) => { if (IsSimple(i)) { AddToListBox(listBox2, i); counter++; } }); }));
+            textBox4.Text = counter.ToString();
+        }
+
+        private void AddToListBox(ListBox listBox, object value)
+        {
+            if (listBox.InvokeRequired)
+            {
+                listBox.Invoke(() => AddToListBox(listBox, value));
+            }
+            else
+            {
+                listBox.Items.Add(value);
+            }
         }
     }
 }
