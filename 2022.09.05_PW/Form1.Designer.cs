77a6864fede8d3cfb18ff439b3c67c83eb667713
Rhoxolan <pba17mail@gmail.com>
2022-09-10 00:56:39 +0300

    Add to PW

--- 2022.09.05_PW/Form1.Designer.cs
+++ 2022.09.05_PW/Form1.Designer.cs
@@ -31,7 +31,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -58,25 +62,62 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 15);
+            this.label2.Size = new System.Drawing.Size(200, 15);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Уникальные значения:";
+            this.label2.Text = "Количество уникальных значений:";
             // 
-            // listBox1
+            // textBox1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(12, 71);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(144, 184);
-            this.listBox1.TabIndex = 3;
+            this.textBox1.Location = new System.Drawing.Point(12, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(218, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(64, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(246, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Максимальная длина последовательности:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(218, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Найти файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Задание 1:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -92,6 +133,10 @@
         private Label label1;
         private Button button1;
         private Label label2;
-        private ListBox listBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Button button2;
+        private Label label4;
     }
 }
--- 2022.09.05_PW/Form1.cs
+++ 2022.09.05_PW/Form1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 
 namespace _2022._09._05_PW
@@ -22,6 +23,56 @@ namespace _2022._09._05_PW
                     list.Add(Convert.ToInt32(line));
                 }
             }
+            var uniquelist = (from i in list.AsParallel()
+                              where IsUnique(list, i)
+                              select i).ToList();
+            textBox1.Text = uniquelist.Count.ToString();
+        }
+
+        private bool IsUnique(List<int> list, int i)
+        {
+            Thread.Sleep(1000); //Исскуственное замедление для отладки - если метод на запрос не параллельный, мы будем последователньо ждать выполнения Thread.Sleep
+            int counter = 0;
+            foreach (int item in list)
+            {
+                if(item == i)
+                {
+                    counter++;
+                }
+            }
+            if(counter == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<int> list = new();
+            OpenFileDialog ofd = new();
+            ofd.Filter = "Text Files(*.txt)|*.txt";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string line in File.ReadLines(ofd.FileName))
+                {
+                    list.Add(Convert.ToInt32(line));
+                }
+            }
+            var uniquelist = (from i in list.AsParallel().AsOrdered() select i).ToList();
+            List<int> counts = new() { 1 };
+            for (int i = 1; i < uniquelist.Count; i++)
+            {
+                if (uniquelist[i] > uniquelist[i-1] && uniquelist[i - 1] > 0)
+                {
+                    counts[^1]++;
+                }
+                else
+                {
+                    counts.Add(1);
+                }
+            }
+            textBox2.Text = counts.Max().ToString();
         }
     }
 }
